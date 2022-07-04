7ff525caf7f5f120651b2c0bfc27a612a7e39210
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-04 15:50:31 -0300

    Desafio 4 Concluido

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function createPixel(pixel, borda) {
+  let pixelBord = document.getElementById('pixel-board');
+  pixelBord.style.width = borda;
 
+  for (let index = 0; index < pixel; index++) {
+    let createDiv = document.createElement('div');
+    createDiv.className = 'pixel';
+    pixelBord.appendChild(createDiv);
+  }
+}
+
+createPixel(25, '410px');
