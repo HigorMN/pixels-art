6305b7ed6b5132557048b5360eb744f49f14b804
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-04 18:21:52 -0300

    Desafio 8 Concluido

--- script.js
+++ script.js
@@ -18,15 +18,16 @@ function colorSelected() {
   let colorRed = document.getElementById('red');
   let colorBlue = document.getElementById('blue');
   let colorGreen = document.getElementById('green');
-  colorBlack.classList.add('selected')
+  colorBlack.classList.add('selected');
 
   for (let index = 0; index < colors.length; index += 1) {
     colors[index].addEventListener('click', function (event) {
       if (colorsSelected.length === 1) {
-        colorBlack.className = 'color'
-        colorRed.className = 'color'
-        colorBlue.className = 'color'
-        colorGreen.className = 'color'
+        console.log(event.target.id);
+        colorBlack.className = 'color';
+        colorRed.className = 'color';
+        colorBlue.className = 'color';
+        colorGreen.className = 'color';
         event.target.className = 'color selected';
       }
     });
@@ -34,3 +35,26 @@ function colorSelected() {
 }
 
 colorSelected();
+
+let colorsSelected = document.getElementsByClassName('color selected');
+function pixelCores() {
+  let pixelClass = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixelClass.length; index += 1) {
+    pixelClass[index].addEventListener('click', function (event) {
+        if (colorsSelected.black) {
+            event.target.id = 'black'
+        }
+        if (colorsSelected.red) {
+            event.target.id = 'red'
+        }
+        if (colorsSelected.blue) {
+            event.target.id = 'blue'
+        }
+        if (colorsSelected.green) {
+            event.target.id = 'green'
+        }
+    });
+  }
+}
+
+pixelCores()
